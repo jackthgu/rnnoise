92ecc3a8da568f59db650da427130c2bcf45916b
jackgu <gestoru@gmail.com>
2020-11-14 19:24:51 +0900

    d

--- examples/do_noise.sh
+++ examples/do_noise.sh
@@ -3,5 +3,6 @@
 BIN="./rnnoise_demo"
 FFMPEG="ffmpeg"
 
-ffmpeg -y  -i input.mp4  -acodec pcm_s16le -f s16le -ac 1 -ar 48000 output.pcm
-ffmpeg -f s16le -ar 48000 -ac 2 -i "$1.raw" "$1.wav"
+ffmpeg -y  -i "$1.mp4"  -acodec pcm_s16le -f s16le -ac 1 -ar 48000 "$1.pcm"
+$BIN "$1.pcm" "$1.raw"
+ffmpeg -f s16le -ar 48000 -ac 1 -i "$1.raw" "$1.wav"
